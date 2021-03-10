182b6e6c9c602a8545d81393192df6aafa668bb7
Dougsd <douglas_schmitzdupski@hotmail.com>
2021-03-10 02:36:00 -0300

    Conclusão de configuração da galeria com adição de JS

--- javaScript/galeri.js
+++ javaScript/galeri.js
@@ -17,3 +17,21 @@ function nextFrame(){
     
 }
 
+function backFrame(){
+    let doc = document.getElementById("idImg");
+    let link = doc.getAttribute("src");
+    if(link == "../img/passo1.png"){
+        doc.setAttribute("src","../img/passo6.png");
+    }else if(link == "../img/passo2.png"){
+        doc.setAttribute("src","../img/passo1.png");
+    }else if(link == "../img/passo3.png"){
+        doc.setAttribute("src","../img/passo2.png");
+    }else if(link == "../img/passo4.png"){
+        doc.setAttribute("src","../img/passo3.png");
+    }else if(link == "../img/passo5.png"){
+        doc.setAttribute("src","../img/passo4.png");
+    }else if(link == "../img/passo6.png"){
+        doc.setAttribute("src","../img/passo5.png");
+    }
+    
+}
